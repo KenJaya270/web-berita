5bb34472ad8ae77abbcdaa54a9486daef27f5991
= <=>
2022-08-30 19:25:20 +0800

    merapikan file

---
+++ app/controllers/User.php
@@ -0,0 +1,11 @@
+<?php
+    class User extends Controller{
+        public function index(){
+            echo "User/index";
+        }
+
+        public function profile($nama = "Linux", $pekerjaan = "Devs"){
+            echo "Salam kenal saya $nama, saya seorang $pekerjaan"
+        }
+    }
+?>
