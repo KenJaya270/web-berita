899428f686dfc3a90aab01c8008f4d5f75290be6
exypnsvgirlyx <contactbintangdw@gmail.com>
2022-08-31 10:04:07 +0800

    save

---
+++ app/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+    class Home extends Controller{
+        
+        public function index(){
+            $data['judul'] = "Home";
+
+            $this->view('template/header');
+
+            $this->view('home/index');
+
+            $this->view('template/footer');
+        }
+
+    }
+?>
---
+++ app/view/home/index.php
@@ -0,0 +1,51 @@
+
+        <!--Berita Utama-->
+        <div class="content">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-8">
+                        <div class="card">
+                            <img class="card-img-top" src="img/berita1.jpg  " alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title" name="judul">megawati menaikan pajak bensin bbm</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Lihat Berita</a>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+                <!--Card  Start-->
+                <div class="row jutify-content-center mt-3" name="">
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-12">
+                        <div class="card">
+                            <img class="card-img-top" src="img/berita2.webp" alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">Berita Dhea Onlyfans Terkini Hari Ini</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Lihat Berita</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-12">
+                        <div class="card">
+                            <img class="card-img-top" src="img/berita3.webp  " alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">Apple Watch Series 8 Diprediksi Dapat Mendeteksi Demam</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Lihat Berita</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-12">
+                        <div class="card">
+                            <img class="card-img-top" src="img/berita5.jpg" alt="Card image cap">
+                            <div class="card-body">
+                              <h5 class="card-title">Steam Diblokir, Pemilik Akun @razfaren Rugi Rp 300 Juta dan Minta Ganti Rugi ke Kemenkominfo</h5>
+                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                              <a href="#" class="btn btn-primary">Lihat Berita</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
